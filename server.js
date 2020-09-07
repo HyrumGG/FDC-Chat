f9c0359b54853f600c1ce4cf46bc5c2f806f3e5d
Hyrum Nantais <truekohlar@gmail.com>
2020-09-07 00:57:24 -0400

    MVP upload

--- server.js
+++ server.js
@@ -7,17 +7,49 @@ http.listen(3000);
 
 app.use(express.static("public"));
 
-const uri = "mongodb+srv://admin:FCXuaT7DA45SM4N@cluster0.il8as.mongodb.net/messages?retryWrites=true&w=majority";
-
-MongoClient.connect(uri, function(err, client) {
-    const db = client.db('messages');
+MongoClient.connect(process.env.MONGODB_URI, function(err, client) {
+    const db = client.db('messages').collection('messages');
+    connectedClients = [];
     
     io.sockets.on("connection", function(socket) {
-        console.log('User Connected');
+        console.log(`User Connected`);
+        if (connectedClients.indexOf(socket) === -1) {
+            connectedClients.push(socket);
+        }
+
+        db.find().toArray().then(function (docs) {
+            socket.emit("retrievePast", docs);
+        });
+
         socket.on("message", function(msg) {
-            db.insertOne({text:message});
+            db.insertOne({text:msg});
             socket.broadcast.emit("message", msg);
         });
-    })
+
+        socket.on("username", function (username) {
+            socket.name = username;
+            socket.broadcast.emit("username", {
+                username: username,
+                id: socket.id
+            });
+        });
+
+        socket.on("GetClients", function (nothing, callback) {
+            var usersList = connectedClients.map(function (item) {
+                return {
+                    id: item.id,
+                    username: item.name
+                }
+            });
+            callback(usersList);
+        });
+
+        socket.on('disconnect', function() {
+            console.log('User Disconnected!');
+            var index = connectedClients.indexOf(socket);
+            connectedClients.splice(index, 1);
+            socket.broadcast.emit("UserDisconnect");
+        });
+    });
 })
 
