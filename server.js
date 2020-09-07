4c42d6a23fd54d8868389faa21fc27fddf3fb925
Hyrum Nantais <truekohlar@gmail.com>
2020-09-07 15:01:45 -0400

    added network access to mongo and mongo code back

--- server.js
+++ server.js
@@ -2,25 +2,30 @@ const express = require("express");
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io').listen(http);
+const MongoClient = require('mongodb').MongoClient;
 const PORT = process.env.PORT || 3000;
 http.listen(PORT);
 
 app.use(express.static("public"));
 
-    db = [];
+const uri = "mongodb+srv://admin:FCXuaT7DA45SM4N@cluster0.il8as.mongodb.net/messages?retryWrites=true&w=majority";
+
+MongoClient.connect(process.env.MONGODB_URI || uri, function(err, client) {
+    const db = client.db('messages').collection('messages');
+
     connectedClients = [];
     
     io.sockets.on("connection", function(socket) {
-        console.log(`User Connected`);
-
-        socket.emit("retrievePast", db);
-
         if (connectedClients.indexOf(socket) === -1) {
             connectedClients.push(socket);
         }
 
+        db.find().toArray().then(function (docs) {
+            socket.emit("retrievePast", docs);
+        });;
+
         socket.on("message", function(msg) {
-            db.push({text:msg});
+            db.insertOne({text:msg});
             socket.broadcast.emit("message", msg);
         });
 
@@ -43,10 +48,9 @@ app.use(express.static("public"));
         });
 
         socket.on('disconnect', function() {
-            console.log('User Disconnected!');
             var index = connectedClients.indexOf(socket);
             connectedClients.splice(index, 1);
             socket.broadcast.emit("UserDisconnect");
         });
     });
-
+})
